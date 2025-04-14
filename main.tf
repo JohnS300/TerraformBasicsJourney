32d83416ab0312a371a222dacc34b3d6b8642634
JohnS300 <johnsofokleous@gmail.com>
2025-04-14 23:49:51 +0300

    Modification to try and make this terier work

--- main.tf
+++ main.tf
@@ -4,5 +4,9 @@ provider "aws" {
 
 resource "aws_s3_bucket" "terraform_bucket" {
     bucket = "terraform_bucket_1"
+}
+
+resource "aws_s3_bucket_acl" "terraform_acl" {
+    bucket = aws_s3_bucket.terraform_bucket
     acl = "private"
 }
